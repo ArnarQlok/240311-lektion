72c2fdf2a33d33ef28c75e74773160c8773a7eae
Arnar Johannsson <arnar.johannsson@qlok.se>
2024-03-11 14:56:04 +0100

    Rendera listor i React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import Skill from "./Skill";
+
+function App() {
+  return <Skill />;
+}
+
+export default App;
---
+++ src/Skill.jsx
@@ -0,0 +1,35 @@
+//importera useState
+import { useState } from "react";
+
+export default function Skill() {
+  // skapa ett state för skills
+  const [skills, setSkills] = useState(["JavaScript", "TypeScript", "React"]);
+
+  // Add skills
+  function handleAddSkill() {
+    const newSkill = document.getElementById("skillInput").value;
+    document.getElementById("skillInput").value = "";
+    setSkills((prevSkills) => [...prevSkills, newSkill]);
+  }
+
+  // Remove skills
+  function handleRemoveSkill(index) {
+    setSkills(skills.filter((skill, i) => i !== index));
+  }
+
+  return (
+    <div>
+      <h2>My Skills</h2>
+      <ul>
+        {skills.map((skill, index) => (
+          <li key={index} onDoubleClick={() => handleRemoveSkill(index)}>
+            {skill}
+          </li>
+        ))}
+      </ul>
+      <input id="skillInput" type="text" placeholder="Enter skill" />
+
+      <button onClick={handleAddSkill}>Add skill</button>
+    </div>
+  );
+}
